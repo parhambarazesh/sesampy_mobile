008a140656fb00b3d732c1fa6b9684251475224c
parhambarazesh <parham.barazesh@gmail.com>
2023-03-26 01:14:13 +0100

    download connectors change color of downloaded connector components

--- Controller/connector_screen.py
+++ Controller/connector_screen.py
@@ -1,14 +1,18 @@
 import importlib
 import webbrowser
 from functools import partial
-
+import git
+import os
 from kivy.metrics import dp
 from kivy.properties import StringProperty
+from kivy.uix.popup import Popup
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFloatingActionButtonSpeedDial, MDFlatButton, MDRaisedButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.dropdownitem import MDDropDownItem
 from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.progressbar import MDProgressBar
+from kivymd.uix.spinner import MDSpinner
 from kivymd.uix.textfield import MDTextField
 
 import View.ConnectorScreen.connector_screen
@@ -34,6 +38,7 @@ class ConnectorScreenController:
         self.view = View.ConnectorScreen.connector_screen.ConnectorScreenView(controller=self, model=self.model)
 
         self.speed_dial_data = model.speed_dial_data
+        self.downloaded_connectors = model.downloaded_connectors
         speed_dial = MDFloatingActionButtonSpeedDial()
         speed_dial.data = self.speed_dial_data
         speed_dial.root_button_anim = True
@@ -44,30 +49,40 @@ class ConnectorScreenController:
             self.view.ids.connector_list.add_widget(
                 TwoLineAvatarIconListItem(
                     IconLeftWidget(
-                        icon="cog"
+                        id="icon_left",
+                        icon="cog",
+                        theme_text_color="Custom",
+                        text_color="blue" if connector[0] in self.downloaded_connectors else "red"
                     ),
                     IconRightWidget(
+                        id="icon_right",
                         icon="download",
-                        on_release=lambda x: self.download_connector(connector)
+                        on_release=partial(self.download_connector, connector),
+                        theme_text_color = "Custom",
+                        text_color = "blue" if connector[0] in self.downloaded_connectors else "red"
                     ),
                     text=connector[0],
                     secondary_text=connector[1],
-                    on_release=partial(self.open_connector, connector)
+                    on_release=partial(self.open_connector, connector),
+                    id=f"connector_item_{connector[0]}",
                 )
             )
 
         self.view.add_widget(speed_dial)
 
-
-
-
     def get_view(self) -> View.ConnectorScreen.connector_screen:
         return self.view
 
-    def download_connector(self, connector):
-        webbrowser.open_new_tab(connector[1])
+    def download_connector(self, connector, *args):
+        os.chdir('core')
+        git.Git().clone(connector[1])
+        os.chdir('..')
+        connector_object_list=[{"id": x.id, "connector": x} for x in self.view.ids.connector_list.children]
+        downloaded_connector=[d for d in connector_object_list if d["id"] == f"connector_item_{connector[0]}"][0]
+        downloaded_connector["connector"].ids.icon_right.text_color = [0, 0, 1, 1]
+        downloaded_connector["connector"].ids.icon_left.text_color = [0, 0, 1, 1]
 
-    def open_connector(self, connector,*args):
+    def open_connector(self, connector, *args):
         webbrowser.open_new_tab(connector[1])
 
     def callback_for_menu_items(self, instance):
