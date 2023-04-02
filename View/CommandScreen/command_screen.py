2a055098ba6a06364b996b544630865261e921b8
parhambarazesh <parham.barazesh@gmail.com>
2023-04-02 09:19:48 +0200

    update sesam commands

--- View/CommandScreen/command_screen.py
+++ View/CommandScreen/command_screen.py
@@ -61,11 +61,17 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_authenticate.active = True
         else:
             self.ids.spinner_authenticate.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def authenticate(self):
-        for i in range(5):
-            print("Authenticate: " + str(i))
-            time.sleep(1)
-        self.spinner_authenticate_toggle()
+        self.args_dict["command"] = "authenticate"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
+        self.spinner_download_toggle()
+
     def authenticate_thread(self):
         self.spinner_authenticate_toggle()
         threading.Thread(target=(self.authenticate)).start()
@@ -79,14 +85,12 @@ class CommandScreenView(BaseScreenView):
             self.ids.log.text = self.logs
             self.ids.logs.badge_icon = "numeric-1"
     def download(self):
-        # save logs from cli to file
-        logging.basicConfig(filename='output.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
-
-
         self.args_dict["command"] = "download"
         self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
         start_cli(self.args_dict)
-        self.logs = "Command Completed"
+        os.chdir(os.path.join(ROOT_DIR,"core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_download_toggle()
 
     def download_thread(self):
@@ -100,16 +104,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_upload.active = True
         else:
             self.ids.spinner_upload.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def upload(self):
-        with open('output.log', 'w') as f:
-            sys.stdout = f  # Redirect stdout to the file
-             # Call the function
-            # Reset stdout to default
-            for i in range(5):
-                # show logs in console with logging
-                logging.debug('Finished reading data.')
-                time.sleep(1)
-            sys.stdout = sys.__stdout__
+        self.args_dict["command"] = "upload"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_upload_toggle()
     def upload_thread(self):
         self.spinner_upload_toggle()
@@ -121,10 +124,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_status.active = True
         else:
             self.ids.spinner_status.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def status(self):
-        for i in range(5):
-            print("Status: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "status"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_status_toggle()
     def status_thread(self):
         self.spinner_status_toggle()
@@ -136,11 +144,16 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_verify.active = True
         else:
             self.ids.spinner_verify.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
 
     def verify(self):
-        for i in range(5):
-            print("Verify: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "verify"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_verify_toggle()
 
     def verify_thread(self):
@@ -153,10 +166,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_test.active = True
         else:
             self.ids.spinner_test.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def test(self):
-        for i in range(5):
-            print("Test: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "test"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_test_toggle()
     def test_thread(self):
         self.spinner_test_toggle()
@@ -169,10 +187,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_stop.active = True
         else:
             self.ids.spinner_stop.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def stop(self):
-        for i in range(5):
-            print("Stop: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "stop"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_stop_toggle()
     def stop_thread(self):
         self.spinner_stop_toggle()
@@ -184,10 +207,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_run.active = True
         else:
             self.ids.spinner_run.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def run(self):
-        for i in range(5):
-            print("Run: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "run"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_run_toggle()
     def run_thread(self):
         self.spinner_run_toggle()
@@ -199,11 +227,16 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_wipe.active = True
         else:
             self.ids.spinner_wipe.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def wipe(self):
-        for i in range(5):
-            print("Wipe: " + str(i))
-            time.sleep(1)
-        self.spinner_wipe_toggle()
+        self.args_dict["command"] = "wipe"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
+        self.spinner_run_toggle()
     def wipe_thread(self):
         self.spinner_wipe_toggle()
         threading.Thread(target=(self.wipe)).start()
@@ -214,10 +247,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_restart.active = True
         else:
             self.ids.spinner_restart.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def restart(self):
-        for i in range(5):
-            print("Restart: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "restart"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_restart_toggle()
     def restart_thread(self):
         self.spinner_restart_toggle()
@@ -229,11 +267,16 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_reset.active = True
         else:
             self.ids.spinner_reset.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def reset(self):
-        for i in range(5):
-            print("Reset: " + str(i))
-            time.sleep(1)
-        self.spinner_reset_toggle()
+        self.args_dict["command"] = "reset"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
+        self.spinner_run_toggle()
     def reset_thread(self):
         self.spinner_reset_toggle()
         threading.Thread(target=(self.reset)).start()
@@ -244,10 +287,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_convert.active = True
         else:
             self.ids.spinner_convert.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def convert(self):
-        for i in range(5):
-            print("Convert: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "convert"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_convert_toggle()
     def convert_thread(self):
         self.spinner_convert_toggle()
@@ -259,10 +307,15 @@ class CommandScreenView(BaseScreenView):
             self.ids.spinner_dump.active = True
         else:
             self.ids.spinner_dump.active = False
+            self.ids.log.text = self.logs
+            self.ids.logs.badge_icon = "numeric-1"
     def dump(self):
-        for i in range(5):
-            print("Dump: " + str(i))
-            time.sleep(1)
+        self.args_dict["command"] = "dump"
+        self.args_dict["connector_dir"] = f"{self.current_connector}-connector"
+        start_cli(self.args_dict)
+        os.chdir(os.path.join(ROOT_DIR, "core"))
+        with open('output.log', 'r') as f:
+            self.logs = f.read()
         self.spinner_dump_toggle()
     def dump_thread(self):
         self.spinner_dump_toggle()
